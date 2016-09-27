35439084a549f09cc2e9cd16ac7dff008668b269
jordanzet <jordanzet@gmail.com>
2016-09-27 13:07:34 -0500

    Web econ inicial

--- application/config/routes.php
+++ application/config/routes.php
@@ -49,6 +49,13 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | Examples:	my-controller/index	-> my_controller/index
 |		my-controller/my-method	-> my_controller/my_method
 */
-$route['default_controller'] = 'welcome';
+$route['default_controller'] = 'pages/index';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
+
+$route['conferencias'] = 'pages/conferencias';
+$route['feria'] = 'pages/feria';
+$route['talleres'] = 'pages/talleres';
+
+#$route['(:any)'] = 'pages/view/$1';
+
---
+++ application/controllers/Pages.php
@@ -0,0 +1,19 @@
+<?php
+class Pages extends CI_Controller {
+
+	public function index(){
+		$this->load->view('econ/index');
+	}
+
+	function conferencias(){
+		$this->load->view('econ/conferencias');
+	}
+		
+	function feria(){
+		$this->load->view('econ/feria');
+	}
+		
+	function talleres(){
+		$this->load->view('econ/talleres');
+	}
+}
