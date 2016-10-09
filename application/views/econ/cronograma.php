6c03b0939d18f81cfca23f998dd3eb2e12c84940
Kedein Rodriguez Gatica <krodriguezg@uni.pe>
2016-10-09 03:57:33 -0500

    cronograma y encuentro cientifico

--- application/views/econ/cronograma.php
+++ application/views/econ/cronograma.php
@@ -1,28 +1,28 @@
 <!DOCTYPE HTML>
 <html>
 	<head>
-		<title>Cronograma</title>
+		<title>Conferencias</title>
 		<meta charset="utf-8" />
 		<meta name="viewport" content="width=device-width, initial-scale=1" />
 		<meta name="description" content="ECON reune a expertos nacionales e internacionales de electronica, brinda un espacio para la difusión  de los avances tecnológicos orientados a la electrónica">
-		<meta name="keywords" content="ECON, ECON UNI 2016, Congreso, Electronica, tecnología, CTIC, UNI">
-		<!--[if lte IE 8]><script src="assets/js/ie/html5shiv.js"></script><![endif]-->
-		<link rel="stylesheet" href="assets/css/main.css" />
-		<!--[if lte IE 9]><link rel="stylesheet" href="assets/css/ie9.css" /><![endif]-->
-		<!--[if lte IE 8]><link rel="stylesheet" href="assets/css/ie8.css" /><![endif]-->
-		<link  rel= "apple-touch-icon"  sizes= "57x57"  href= "images/favicon/apple-icon-57x57.png" > 
-		<link  rel= "apple-touch-icon"  sizes= "60x60"  href= "images/favicon/apple-icon-60x60.png" > 
-		<link  rel= "apple-touch-icon"  sizes= "72x72"  href= "images/favicon/apple-icon-72x72.png" > 
-		<link  rel= "apple-touch-icon"  sizes= "76x76"  href= "images/favicon/apple-icon-76x76.png" > 
-		<link  rel= "apple-touch-icon"  sizes= "114x114"  href= "images/favicon/apple-icon-114x114.png" > 
-		<link  rel= "apple-touch-icon"  sizes= "120x120"  href= "images/favicon/apple-icon-120x120.png" > 
-		<link  rel= "apple-touch-icon"  sizes= "144x144"  href= "images/favicon/apple-icon-144x144.png" > 
-		<link  rel= "apple-touch-icon"  sizes= "152x152"  href= "images/favicon/apple-icon-152x152.png" > 
-		<link  rel= "apple-touch-icon"  sizes= "180x180"  href= "images/favicon/apple-icon-180x180.png" > 
+		<meta name="keywords" content="ECON, Congreso, Electronica, tecnología, CTIC, UNI">
+		<!--[if lte IE 8]><script src="<?php echo base_url();?>/assets/js/ie/html5shiv.js"></script><![endif]-->
+		<link rel="stylesheet" href="<?php echo base_url();?>/assets/css/main.css" />
+		<!--[if lte IE 9]><link rel="stylesheet" href="<?php echo base_url();?>/assets/css/ie9.css" /><![endif]-->
+		<!--[if lte IE 8]><link rel="stylesheet" href="<?php echo base_url();?>/assets/css/ie8.css" /><![endif]-->
+		<link  rel= "apple-touch-icon"  sizes= "57x57"  href= "<?php echo base_url();?>/images/favicon/apple-icon-57x57.png" > 
+		<link  rel= "apple-touch-icon"  sizes= "60x60"  href= "<?php echo base_url();?>/images/favicon/apple-icon-60x60.png" > 
+		<link  rel= "apple-touch-icon"  sizes= "72x72"  href= "<?php echo base_url();?>/images/favicon/apple-icon-72x72.png" > 
+		<link  rel= "apple-touch-icon"  sizes= "76x76"  href= "<?php echo base_url();?>/images/favicon/apple-icon-76x76.png" > 
+		<link  rel= "apple-touch-icon"  sizes= "114x114"  href= "<?php echo base_url();?>/images/favicon/apple-icon-114x114.png" > 
+		<link  rel= "apple-touch-icon"  sizes= "120x120"  href= "<?php echo base_url();?>/images/favicon/apple-icon-120x120.png" > 
+		<link  rel= "apple-touch-icon"  sizes= "144x144"  href= "<?php echo base_url();?>/images/favicon/apple-icon-144x144.png" > 
+		<link  rel= "apple-touch-icon"  sizes= "152x152"  href= "<?php echo base_url();?>/images/favicon/apple-icon-152x152.png" > 
+		<link  rel= "apple-touch-icon"  sizes= "180x180"  href= "<?php echo base_url();?>/images/favicon/apple-icon-180x180.png" > 
 		<link  rel= "icon"  type= "image/png"  sizes= "192x192"   href= "/android-icon-192x192.png" > 
-		<link  rel= "icon"  type= "image/png"  sizes= "32x32"  href= "images/favicon/favicon-32x32.png" > 
-		<link  rel= "icon"  type= "image/png"  sizes= "96x96"  href= "images/favicon/favicon-96x96.png" > 
-		<link  rel= "icon"  type= "image/png"  sizes= "16x16"  href= "images/favicon/favicon-16x16.png" > 
+		<link  rel= "icon"  type= "image/png"  sizes= "32x32"  href= "<?php echo base_url();?>/images/favicon/favicon-32x32.png" > 
+		<link  rel= "icon"  type= "image/png"  sizes= "96x96"  href= "<?php echo base_url();?>/images/favicon/favicon-96x96.png" > 
+		<link  rel= "icon"  type= "image/png"  sizes= "16x16"  href= "<?php echo base_url();?>/images/favicon/favicon-16x16.png" > 
 		<link  rel= "manifest"  href= "/manifest.json" > 
 		<meta  name= "msapplication-TileColor"  content= "#ffffff" > 
 		<meta  name= "msapplication-TileImage"  content= "/ms-icon-144x144.png" > 
